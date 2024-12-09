0511ff3ef210c6cb79f469c1cde015f648e89b54
m6dusa <roman.smartlogic@gmail.com>
2024-12-09 03:10:11 +0100

    fixed the issue with coins using the same shared animation resource.

--- Scripts/Coin.cs
+++ Scripts/Coin.cs
@@ -6,17 +6,36 @@ public partial class Coin : Area2D, IScorable
 {
 	[Export] private int _score;
 	[Export] private float _heatbarProgress;
-	[Export] private bool _isSwaying = false;
+	[Export] private bool _isSwaying;
+	[Export] private bool _otherWay;
+	[Export] private Animation _animation;
 	public event Action<OnScoreUpdatedEventArgs> ScoreChanged;
 	
 	private AnimationPlayer _animationPlayer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		BodyEntered += OnAreaEnteredByPlayer;
-		_animationPlayer.Play("Coin Movement");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		#region Animation Update
+		//This code is required to create and assign an animation that won't be shared across multiple entities of coin node.
+		//see details https://github.com/godotengine/godot/issues/82421
+		var animLibrary = new AnimationLibrary();
+		var newAnimation = (Animation)_animation.Duplicate();
+		var animName = $"CoinAnimation {Guid.NewGuid().ToString()}";
+		var animLibName = "animLibrary";
+		animLibrary.AddAnimation(animName, newAnimation);
+		_animationPlayer.AddAnimationLibrary(animLibName, animLibrary);
+		//animation name consists of Libraryname and animation name. "exampleLib/exampleAnim"
+		_animationPlayer.GetAnimation($"{animLibName}/{animName}").TrackSetEnabled(1, _isSwaying);
+		_animationPlayer.Play($"{animLibName}/{animName}");
+		//each coin will start randomly between 0 and 1 second frame.
 		_animationPlayer.Advance(GD.RandRange(0f,1f));
+
+			#endregion
+		
+		
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
